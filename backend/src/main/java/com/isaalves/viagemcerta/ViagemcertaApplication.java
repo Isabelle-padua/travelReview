e828a64b13bad1d2c2bae6b018788e9c446c50a3
Isabelle Padua <isaalvespadua@gmail.com>
2023-06-07 14:42:06 -0300

    first commit

---
+++ backend/src/main/java/com/isaalves/viagemcerta/ViagemcertaApplication.java
@@ -0,0 +1,13 @@
+package com.isaalves.viagemcerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViagemcertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViagemcertaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/isaalves/viagemcerta/ViagemcertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaalves.viagemcerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViagemcertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
